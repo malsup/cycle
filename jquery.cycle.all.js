5cb4825e012797519d869a30436a8a49995804d0
malsup <malsup@gmail.com>
2011-03-07 21:18:47 -0500

    v2.97 - sort the options list (yeah, that's right)

--- jquery.cycle.all.js
+++ jquery.cycle.all.js
@@ -2,14 +2,14 @@
  * jQuery Cycle Plugin (with Transition Definitions)
  * Examples and documentation at: http://jquery.malsup.com/cycle/
  * Copyright (c) 2007-2010 M. Alsup
- * Version: 2.96 (11-FEB-2011)
+ * Version: 2.97 (14-FEB-2011)
  * Dual licensed under the MIT and GPL licenses.
  * http://jquery.malsup.com/license.html
  * Requires: jQuery v1.3.2 or later
  */
 ;(function($) {
 
-var ver = '2.96';
+var ver = '2.97';
 
 // if $.support is not defined (pre jQuery 1.3) add what I need
 if ($.support == undefined) {
@@ -869,61 +869,59 @@ $.fn.cycle.ver = function() { return ver; };
 
 // override these globally if you like (they are all optional)
 $.fn.cycle.defaults = {
-	fx:			  'fade', // name of transition effect (or comma separated names, ex: 'fade,scrollUp,shuffle')
-	timeout:	   4000,  // milliseconds between slide transitions (0 to disable auto advance)
-	timeoutFn:     null,  // callback for determining per-slide timeout value:  function(currSlideElement, nextSlideElement, options, forwardFlag)
+	activePagerClass: 'activeSlide', // class name used for the active pager link
+	after:		   null,  // transition callback (scope set to element that was shown):  function(currSlideElement, nextSlideElement, options, forwardFlag)
+	allowPagerClickBubble: false, // allows or prevents click event on pager anchors from bubbling
+	animIn:		   null,  // properties that define how the slide animates in
+	animOut:	   null,  // properties that define how the slide animates out
+	autostop:	   0,	  // true to end slideshow after X transitions (where X == slide count)
+	autostopCount: 0,	  // number of transitions (optionally used with autostop to define X)
+	backwards:     false, // true to start slideshow at last slide and move backwards through the stack
+	before:		   null,  // transition callback (scope set to element to be shown):	 function(currSlideElement, nextSlideElement, options, forwardFlag)
+	cleartype:	   !$.support.opacity,  // true if clearType corrections should be applied (for IE)
+	cleartypeNoBg: false, // set to true to disable extra cleartype fixing (leave false to force background color setting on slides)
+	containerResize: 1,	  // resize container to fit largest slide
 	continuous:	   0,	  // true to start next transition immediately after current one completes
+	cssAfter:	   null,  // properties that defined the state of the slide after transitioning out
+	cssBefore:	   null,  // properties that define the initial state of the slide before transitioning in
+	delay:		   0,	  // additional delay (in ms) for first transition (hint: can be negative)
+	easeIn:		   null,  // easing for "in" transition
+	easeOut:	   null,  // easing for "out" transition
+	easing:		   null,  // easing method for both in and out transitions
+	end:		   null,  // callback invoked when the slideshow terminates (use with autostop or nowrap options): function(options)
+	fastOnEvent:   0,	  // force fast transitions when triggered manually (via pager or prev/next); value == time in ms
+	fit:		   0,	  // force slides to fit container
+	fx:			  'fade', // name of transition effect (or comma separated names, ex: 'fade,scrollUp,shuffle')
+	fxFn:		   null,  // function used to control the transition: function(currSlideElement, nextSlideElement, options, afterCalback, forwardFlag)
+	height:		  'auto', // container height
+	manualTrump:   true,  // causes manual transition to stop an active transition instead of being ignored
+	next:		   null,  // selector for element to use as event trigger for next slide
+	nowrap:		   0,	  // true to prevent slideshow from wrapping
+	onPagerEvent:  null,  // callback fn for pager events: function(zeroBasedSlideIndex, slideElement)
+	onPrevNextEvent: null,  // callback fn for prev/next events: function(isNext, zeroBasedSlideIndex, slideElement)
+	pager:		   null,  // selector for element to use as pager container
+	pagerAnchorBuilder: null, // callback fn for building anchor links:  function(index, DOMelement)
+	pagerEvent:	  'click.cycle', // name of event which drives the pager navigation
+	pause:		   0,	  // true to enable "pause on hover"
+	pauseOnPagerHover: 0, // true to pause when hovering over pager link
+	prev:		   null,  // selector for element to use as event trigger for previous slide
+	prevNextEvent:'click.cycle',// event which drives the manual transition to the previous or next slide
+	random:		   0,	  // true for random, false for sequence (not applicable to shuffle fx)
+	randomizeEffects: 1,  // valid when multiple effects are used; true to make the effect sequence random
+	requeueOnImageNotLoaded: true, // requeue the slideshow if any image slides are not yet loaded
+	requeueTimeout: 250,  // ms delay for requeue
+	rev:		   0,	  // causes animations to transition in reverse (for effects that support it such as scrollHorz/scrollVert/shuffle)
+	shuffle:	   null,  // coords for shuffle animation, ex: { top:15, left: 200 }
+	slideExpr:	   null,  // expression for selecting slides (if something other than all children is required)
+	slideResize:   1,     // force slide width/height to fixed size before every transition
 	speed:		   1000,  // speed of the transition (any valid fx speed value)
 	speedIn:	   null,  // speed of the 'in' transition
 	speedOut:	   null,  // speed of the 'out' transition
-	next:		   null,  // selector for element to use as event trigger for next slide
-	prev:		   null,  // selector for element to use as event trigger for previous slide
-//	prevNextClick: null,  // @deprecated; please use onPrevNextEvent instead
-	onPrevNextEvent: null,  // callback fn for prev/next events: function(isNext, zeroBasedSlideIndex, slideElement)
-	prevNextEvent:'click.cycle',// event which drives the manual transition to the previous or next slide
-	pager:		   null,  // selector for element to use as pager container
-	//pagerClick   null,  // @deprecated; please use onPagerEvent instead
-	onPagerEvent:  null,  // callback fn for pager events: function(zeroBasedSlideIndex, slideElement)
-	pagerEvent:	  'click.cycle', // name of event which drives the pager navigation
-	allowPagerClickBubble: false, // allows or prevents click event on pager anchors from bubbling
-	pagerAnchorBuilder: null, // callback fn for building anchor links:  function(index, DOMelement)
-	before:		   null,  // transition callback (scope set to element to be shown):	 function(currSlideElement, nextSlideElement, options, forwardFlag)
-	after:		   null,  // transition callback (scope set to element that was shown):  function(currSlideElement, nextSlideElement, options, forwardFlag)
-	end:		   null,  // callback invoked when the slideshow terminates (use with autostop or nowrap options): function(options)
-	easing:		   null,  // easing method for both in and out transitions
-	easeIn:		   null,  // easing for "in" transition
-	easeOut:	   null,  // easing for "out" transition
-	shuffle:	   null,  // coords for shuffle animation, ex: { top:15, left: 200 }
-	animIn:		   null,  // properties that define how the slide animates in
-	animOut:	   null,  // properties that define how the slide animates out
-	cssBefore:	   null,  // properties that define the initial state of the slide before transitioning in
-	cssAfter:	   null,  // properties that defined the state of the slide after transitioning out
-	fxFn:		   null,  // function used to control the transition: function(currSlideElement, nextSlideElement, options, afterCalback, forwardFlag)
-	height:		  'auto', // container height
 	startingSlide: 0,	  // zero-based index of the first slide to be displayed
 	sync:		   1,	  // true if in/out transitions should occur simultaneously
-	random:		   0,	  // true for random, false for sequence (not applicable to shuffle fx)
-	fit:		   0,	  // force slides to fit container
-	containerResize: 1,	  // resize container to fit largest slide
-	slideResize:   1,     // force slide width/height to fixed size before every transition
-	pause:		   0,	  // true to enable "pause on hover"
-	pauseOnPagerHover: 0, // true to pause when hovering over pager link
-	autostop:	   0,	  // true to end slideshow after X transitions (where X == slide count)
-	autostopCount: 0,	  // number of transitions (optionally used with autostop to define X)
-	delay:		   0,	  // additional delay (in ms) for first transition (hint: can be negative)
-	slideExpr:	   null,  // expression for selecting slides (if something other than all children is required)
-	cleartype:	   !$.support.opacity,  // true if clearType corrections should be applied (for IE)
-	cleartypeNoBg: false, // set to true to disable extra cleartype fixing (leave false to force background color setting on slides)
-	nowrap:		   0,	  // true to prevent slideshow from wrapping
-	fastOnEvent:   0,	  // force fast transitions when triggered manually (via pager or prev/next); value == time in ms
-	randomizeEffects: 1,  // valid when multiple effects are used; true to make the effect sequence random
-	rev:		   0,	  // causes animations to transition in reverse (for effects that support it such as scrollHorz/scrollVert/shuffle)
-	manualTrump:   true,  // causes manual transition to stop an active transition instead of being ignored
-	requeueOnImageNotLoaded: true, // requeue the slideshow if any image slides are not yet loaded
-	requeueTimeout: 250,  // ms delay for requeue
-	activePagerClass: 'activeSlide', // class name used for the active pager link
-	updateActivePagerLink: null, // callback fn invoked to update the active pager link (adds/removes activePagerClass style)
-	backwards:     false  // true to start slideshow at last slide and move backwards through the stack
+	timeout:	   4000,  // milliseconds between slide transitions (0 to disable auto advance)
+	timeoutFn:     null,  // callback for determining per-slide timeout value:  function(currSlideElement, nextSlideElement, options, forwardFlag)
+	updateActivePagerLink: null // callback fn invoked to update the active pager link (adds/removes activePagerClass style)
 };
 
 })(jQuery);
