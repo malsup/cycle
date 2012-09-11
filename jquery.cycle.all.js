02f57037e5ae1b647d5ed4c1ecc983645bb56d7d
malsup <malsup@gmail.com>
2012-09-11 09:08:43 -0400

    merge height fix

--- jquery.cycle.all.js
+++ jquery.cycle.all.js
@@ -2,7 +2,7 @@
  * jQuery Cycle Plugin (with Transition Definitions)
  * Examples and documentation at: http://jquery.malsup.com/cycle/
  * Copyright (c) 2007-2010 M. Alsup
- * Version: 2.9999.5 (10-APR-2012)
+ * Version: 2.9999.6 (11-SEP-2012)
  * Dual licensed under the MIT and GPL licenses.
  * http://jquery.malsup.com/license.html
  * Requires: jQuery v1.3.2 or later
